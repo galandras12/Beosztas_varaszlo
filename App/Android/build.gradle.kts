778360d72c974f63e5bed5e24d3fb42efbaf1c5d
Andrew Gal <galandras12@gmail.com>
2026-08-04 19:20:38 +0200

    Update with Gemini in Android Studio

--- App/Android/build.gradle.kts
+++ App/Android/build.gradle.kts
@@ -1,5 +1,5 @@
 // Root build file: plugin verzió-deklarációk (a tényleges alkalmazás az app/ modulban van).
 plugins {
-    id("com.android.application") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.android.application") version "9.3.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
 }
